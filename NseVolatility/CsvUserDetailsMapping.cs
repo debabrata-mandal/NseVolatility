ad472a12cf4a58899052ef93f9ba712b15d6047a
Debabrata Mandal <Debabrata.Mandal@hyland.com>
2022-02-18 23:26:54 +0530

    feat: basic difference is implemented

---
+++ NseVolatility/CsvUserDetailsMapping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyCsvParser.Mapping;
+
+namespace NseVolatility
+{
+    public class CsvUserDetailsMapping : CsvMapping<DailyVolatility>
+    {
+        public CsvUserDetailsMapping()
+            : base()
+        {
+            MapProperty(0, x => x.Date);
+            MapProperty(1, x => x.Symbol);
+            MapProperty(5, x => x.PreviousDayVolatility);
+            MapProperty(6, x => x.CurrentDayVolatility);
+        }
+    }
+    public class DailyVolatility
+    {
+        public string Date { get; set; }
+        public string Symbol { get; set; }
+        public double PreviousDayVolatility { get; set; }
+        public double CurrentDayVolatility { get; set; }
+    }
+}
--- NseVolatility/Worker.cs
+++ NseVolatility/Worker.cs
@@ -1,24 +1,29 @@
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using TinyCsvParser;
 
 namespace NseVolatility
 {
     public class Worker : BackgroundService
     {
-        private string _day1CSV;
-        private string _day2CSV;
+        private IEnumerable<DailyVolatility> _day1CSV;
+        private IEnumerable<DailyVolatility> _day2CSV;
 
         public Worker()
         {
+
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var input = "X";
+            string input = "X";
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (string.Equals(input, "X", StringComparison.OrdinalIgnoreCase))
@@ -28,27 +33,43 @@ namespace NseVolatility
                         Console.WriteLine("Dates are not valid please try again");
                         continue;
                     }
+                    if (null != _day1CSV && null != _day2CSV)
+                    {
+                        foreach (DailyVolatility item in _day1CSV.OrderBy(n => n.CurrentDayVolatility))
+                        {
+                            DailyVolatility day2Item = _day2CSV.FirstOrDefault(n => string.Equals(n.Symbol, item.Symbol, StringComparison.OrdinalIgnoreCase));
+                            if (null == day2Item)
+                            {
+                                Console.WriteLine($"Error: Symbol {item.Symbol} not found for previous day");
+                            }
+                            double difference = Math.Round((item.CurrentDayVolatility - day2Item.CurrentDayVolatility) * 100, 2);
 
-                    Console.WriteLine($"{_day1CSV}{_day2CSV}");
+                            Console.WriteLine($"{item.Symbol} - {Math.Round(item.CurrentDayVolatility, 4) * 100}%({ difference}%)");
+                        }
+                    }
                 }
                 input = Console.ReadLine();
             }
-
             await Task.CompletedTask;
         }
 
         private bool GetDates()
         {
+            CsvParserOptions csvParserOptions = new(true, ',');
+            CsvUserDetailsMapping csvMapper = new();
+            CsvParser<DailyVolatility> csvParser = new(csvParserOptions, csvMapper);
+
             if (!GetDate("Day1"))
             {
                 return false;
             }
-            _day1CSV = "Day1.csv";
+            _day1CSV = csvParser.ReadFromFile("Day1.csv", Encoding.ASCII).ToList().Select(n => n.Result);
+
             if (!GetDate("Day2"))
             {
                 return false;
             }
-            _day2CSV = "Day2.csv";
+            _day2CSV = csvParser.ReadFromFile("Day2.csv", Encoding.ASCII).ToList().Select(n => n.Result);
             return true;
         }
 
